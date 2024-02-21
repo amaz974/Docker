9bc697e2a587ae6bb00ffb1341e83078779ae496
unknown <alexandre.mazoyer@ynov.com>
2024-02-21 15:50:30 +0100

    ajout du TP2 en pdf et md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Alexandre MAZOYER
 
 ## DOCKER  TP1 
-Vous trouverez au dessus dans le repertoire Compte rendue-TP1 le compte rendu du TP1 en markdow ainsi qu'en PDF.
+Vous trouverez au dessus dans le repertoire Compte rendue-TP1 le compte rendu du TP1 en markdown ainsi qu'en PDF.
+De plus, dans le reperoire Compte rendue-TP2 vous trouverez le TP2 sous forme pdf ainsi que markdown.
--- src/db.config.js
+++ src/db.config.js
@@ -1,16 +1,17 @@
-// Uncomment this block to use sqlite
-module.exports = {
+// Comment this block to disable sqlite
+/*module.exports = {
     dialect: "sqlite",
     storage: "./my-db.sqlite",
-}
+}*/
 
 // Uncomment this block to use mysql
-/*module.exports = {
-    hostname: "",
-    username: "",
-    password: "",
-    database: "",
-    port: 3306
-}*/
+module.exports = {
+    dialect: "mysql",
+    hostname: process.env.DB_HOST || "mysql_container",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "mydatabase",
+    port: process.env.DB_PORT || 3306
+};
 
 // TODO : adapt this file to load parameters from environment variables (process.env.VARIABLE_NAME)
